754af0f66021126741d735f67bd92b9de3b90a77
mothaposuzy <mothaposuzy2@gmail.com>
2023-02-16 13:55:22 -0800

    5-printf.c

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+/**
+ * main - print a c program using printf
+ *
+ * return: 0
+ */
+intr main(void)
+{
+	printf ("with proper grammar, but the outcome is a piece of art,\n");
+	return(0);
+}
