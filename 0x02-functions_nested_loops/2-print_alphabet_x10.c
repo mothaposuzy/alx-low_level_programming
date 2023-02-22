8c075dbc6894b5f206c38adc943904c1d05d7f35
mothaposuzy <mothaposuzy2@gmail.com>
2023-02-21 19:03:49 -0800

    2-print_alphabet_x10.c

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -8,14 +8,14 @@
 
 void print_alphabet_x10(void);
 {
-	int count = 0;
+	int i;
 
-	char letter;
+	char j;
 
-	while (count++ <= 9)
+	for (i = 1; i <= 9; i++)
 	{
-		for (letter = 'a'; letter <= 'z'; letter++)
-			_putchar(letter);
+		for (j = 'a'; j <= 'z'; j++)
+			_putcha(j);
 		_putchar ('\n');
 	}
 }
