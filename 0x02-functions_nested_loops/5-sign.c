3f48410661f45ad277f68ec14b7d5be5703e9db2
mothaposuzy <mothaposuzy2@gmail.com>
2023-02-22 17:40:56 -0800

    5-sign.c

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -2,7 +2,7 @@
 
 /**
  *
- *Print_sign - function that prints signs
+ *print_sign - function that prints signs
  *
  *@n: function parameter
  *
@@ -13,17 +13,17 @@ int print_sign(int n)
 {
 	if (n > 0)
 	{
-		_putchar('+')
+		_putchar('+');
 		return (1);
 	}
 	else if (n == 0)
 	{
-		 _putchar('0')
+		 _putchar('0');
 		return (0);
 	}
 	else
 	{
-		 _putchar('-1')
+		 _putchar('-1');
 		return (-1);
 	}
 }
