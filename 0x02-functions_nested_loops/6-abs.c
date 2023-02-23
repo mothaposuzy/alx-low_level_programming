1c4169d0f338789a90f2dc325a4fa291d42febbf
mothaposuzy <mothaposuzy2@gmail.com>
2023-02-22 19:23:48 -0800

    6-abs.c

--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -1,19 +1,15 @@
 #include "main.h"
 
 /**
- *
  * _abs - checks for absolute value
- *
- * @a: parameter to be checked
- *
+ *@a: the int to be checked
  *Return: a
  */
 
 int _abs(int a)
 {
-	 if (a < 0)
-		 a = -(a);
-	 else if (a >= 0)
-		 a = a;
-	 return (a);
+	if (a >= 0)
+		return (a);
+	else
+		return (-a);
 }
