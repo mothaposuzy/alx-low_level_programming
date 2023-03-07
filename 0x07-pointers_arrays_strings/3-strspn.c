ef5bc0873e4ad261d08bf5a657126096526c55c9
mothaposuzy <mothaposuzy2@gmail.com>
2023-03-06 16:29:50 -0800

    3-strspn.c

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -15,7 +15,8 @@ unsigned int _strspn(char *s, char *accept)
 	for (i = 0; s[i] != '\0'; i++)
 	{
 		check = 0;
-		for (n = 0; accep[n] != '\0'; n++)
+
+		for (n = 0; accept[n] != '\0'; n++)
 		{
 			if (accept[n] == s[i])
 			{
