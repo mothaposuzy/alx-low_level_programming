bee6b54f2dd3198889129789cabefb83a35ff9cb
mothaposuzy <mothaposuzy2@gmail.com>
2023-03-14 16:40:59 -0700

    2-str_concat.c

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -15,12 +15,12 @@ char *str_concat(char *s1, char *s2)
 	char *s;
 
 	if (s1 == NULL)
-		s1 == "";
+		s1 = "";
 
 	if (s2 == NULL)
-		s1 == "";
+		s1 = "";
 
-	while (s[i])
+	while (s1[i])
 		i++;
 
 	while (s[j])
