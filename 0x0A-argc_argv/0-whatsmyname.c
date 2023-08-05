1c697f76596cd1c98b9c172333896ebf232cd56a
mothaposuzy <mothaposuzy2@gmail.com>
2023-08-04 17:01:24 -0700

    0-whatsmyname.c

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,11 +1,12 @@
 #include "main.h"
+#include <stdio.h>
 
 /**
  * main - prints its name
  * @argc: argument counter
  * @argv: argument vector
  *
- * Return:  nothing
+ * Return: 0
 */
 
 int main(int argc, count argv[])
@@ -13,7 +14,8 @@ int main(int argc, count argv[])
 	void(argc);
 
 	_putchar(argv[0]);
+
 	_putchar('\n');
 
-	return ();
+	return (0);
 }
