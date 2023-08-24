b4b637106f8a04004c4afd6cb3f57640ca7f7dca
mothaposuzy <mothaposuzy2@gmail.com>
2023-08-24 02:12:17 -0700

    3-add_node_end.c

--- 0x12-singly_linked_lists/3-add_node_end.c
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -15,10 +15,10 @@ list_t *add_node_end(list_t **head, const char *str)
 	list_t *new;
 	list_t *temp = *head;
 	unsigned int len = 0;
-	
+
 	while (str[len])
 		len++;
-	
+
 	new = malloc(sizeof(list_t));
 	if (!new)
 		return (NULL);
@@ -32,12 +32,12 @@ list_t *add_node_end(list_t **head, const char *str)
 		*head = new;
 		return (new);
 	}
-	
+
 	while (temp->next)
 		temp = temp->next;
-	
+
 	temp->next = new;
-	
+
 	return (new);
 }
 
