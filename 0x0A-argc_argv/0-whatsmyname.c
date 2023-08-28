992cd29cb2b0fc197b5cbb02e6e357180c552e2d
mothaposuzy <mothaposuzy2@gmail.com>
2023-08-27 18:50:10 -0700

    0-whatsmyname.c

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,5 +1,5 @@
-#include "main.h"
 #include <stdio.h>
+#include "main.h"
 
 /**
  * main - prints the name of the program
@@ -8,10 +8,9 @@
  *
  * Return: Always 0 (Success)
  */
-
 int main(int argc __attribute__((unused)), char *argv[])
 {
 	printf("%s\n", *argv);
+
 	return (0);
 }
-
