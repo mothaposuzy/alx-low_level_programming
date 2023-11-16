bda3ddba71237d3030b8be01141fa438693aa663
mothaposuzy <mothaposuzy2@gmail.com>
2023-11-15 19:15:31 -0800

    0-puts_recursion.c

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -9,13 +9,10 @@
 
 void _puts_recursion(char *s)
 {
-	if (*s == '\0')
+	if (*s != '\0')
 	{
-		return (0);
-		_putchar('\n);
+		_putchar(*s);
+		s++;
 	}
-	_puts_recursion(s);
-	s++;
-
-	_putchar(*s);
+		_putchar('\n');
 }
