fd7f359c251aceb43c63ea34c46786929b2fef53
mothaposuzy <mothaposuzy2@gmail.com>
2023-11-17 11:39:55 -0800

    0-whatsmyname.c

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,18 @@
+#include main.h
+
+/**
+ * main - prints the name of the program.
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	const char = "mynameis"
+
+		for (int i = 0, mynameis[i] != '\0'; i++)
+		{
+			_putchar(mynameis[i]);
+		}
+	_putchar('\n');
+	return (0);
+}
