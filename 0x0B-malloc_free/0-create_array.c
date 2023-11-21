7509c84f3bd192625ff6a53df9701fb2ec5b4858
mothaposuzy <mothaposuzy2@gmail.com>
2023-11-21 05:14:20 -0800

    1-strdup.c

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,26 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * create_array - create array of size size and assign char c
+ * @size: size of array
+ * @c: char to assign
+ * Description: create array of size size and assign char c
+ * return: pointer to array, NULL if it fails
+ */
+
+char *create_array(unsigned int size, char c)
+{
+	char *str;
+
+	Unsigned int i;
+
+	str = malloc(sizeof(char) * size)
+		if (Size == 0 || str == NULL)
+			return (NULL);
+
+	for (i = 0; i < size; i++)
+		str[i] = c;
+
+	return (str);
+}
