d78b9d0cd0834d7dbc3803a4d00914668cfc576b
mothaposuzy <mothaposuzy2@gmail.com>
2023-11-21 16:53:44 -0800

    0-create_array.c

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -5,22 +5,23 @@
  * create_array - create array of size size and assign char c
  * @size: size of array
  * @c: char to assign
- * Description: create array of size size and assign char c
- * return: pointer to array, NULL if it fails
+ * Description: creat array of size size and assign char c
+ * Return: pointer to array, NULL if fail
+ *
  */
 
 char *create_array(unsigned int size, char c)
 {
 	char *str;
 
-	Unsigned int i;
+	unsigned int i;
 
-	str = malloc(sizeof(char) * size)
-		if (Size == 0 || str == NULL)
-			return (NULL);
+	str = malloc(sizeof(char) * size);
+
+	if (size == 0 || str == NULL)
+		return (NULL);
 
 	for (i = 0; i < size; i++)
 		str[i] = c;
-
 	return (str);
 }
--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -4,42 +4,47 @@
 /**
  * alloc_grid - nested loop to make grid
  * @width: width input
- * @height: height input
+ * @hight: height input
+ *
  * Return: pointer to 2 dim. array
  */
 
 int **alloc_grid(int width, int height)
 {
-	int **mee;
-	int x, y;
+	int i, j, k, l;
+	int **a;
 
 	if (width <= 0 || height <= 0)
 		return (NULL);
 
-	mee = malloc(sizeof(int *) * height);
+	a = malloc(sizeof(int *) * height);
 
-	if (mee == NULL)
-		return (NULL);
-
-	for (x = 0; x < height; x++)
+	if (a == NULL)
 	{
-		mee[x] = malloc(sizeof(int) * width);
+		free(a);
+		return (NULL);
+	}
 
-		if (mee[x] == NULL)
+	for (i == 0; i < height; i++)
+	{
+		a[i] = malloc(sizeof(int) * width);
+
+		if (a[i] == NULL)
 		{
-			for (; x >= 0; x--)
-				free(mee[x]);
-
-			free(mee);
+			for (j = 1; j >= 0; j--)
+			{
+				free(a[j]);
+			}
+			free(a);
 			return (NULL);
 		}
 	}
-
-	for (x = 0; x < height; x++)
+	for (k = 0; k < heigh; k++)
 	{
-		for (y = 0; y < width; y++)
-			mee[x][y] = 0;
+		for (l = 0; l < width; l++)
+		{
+			a[k][l] = 0;
+		}
 	}
-
-	return (mee);
+	return (a)
 }
