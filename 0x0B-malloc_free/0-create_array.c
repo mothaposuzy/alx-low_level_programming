80cef6038e6d3c6a89bcb5dff8279f37c1f5e4d3
mothaposuzy <mothaposuzy2@gmail.com>
2023-11-21 17:32:01 -0800

    0-create_array.c

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -11,6 +11,7 @@
  */
 
 char *create_array(unsigned int size, char c)
+
 {
 	char *str;
 
@@ -19,9 +20,13 @@ char *create_array(unsigned int size, char c)
 	str = malloc(sizeof(char) * size);
 
 	if (size == 0 || str == NULL)
+
 		return (NULL);
 
 	for (i = 0; i < size; i++)
+
 		str[i] = c;
+
 	return (str);
+
 }
