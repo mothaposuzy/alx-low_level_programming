57194922385153432f6393b6acec40b841a89ad0
mothaposuzy <mothaposuzy2@gmail.com>
2023-11-30 14:09:10 -0800

    0-sum_them_all.c

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -1,9 +1,8 @@
-#ifndef _VARIADIC_FUNCTIONS_
-#define _VARIADIC_FUNCTIONS_
+#ifndef VARIADIC_FUNCTIONS
+#define VARIADIC_FUNCTIONS
 
 #include <stdio.h>
 #include <stdarg.h>
-#include <stddef.h>
 
 int _putchar(char c);
 int sum_them_all(const unsigned int n, ...);
