2ce43d681c9e84e8c7100957dc2e0cb4243c480d
mothaposuzy <mothaposuzy2@gmail.com>
2023-12-22 17:16:00 -0800

    0-binary_to_uint.c

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -21,19 +21,19 @@ unsigned int binary_to_uint(const char *b)
 
 	if (b == NULL)
 		return (0);
-		for (len = 0; b[len] != '\0'; len++)
-			;
-		if (len == 1 && (b[0] == '0' || b[0] == '1'))
-			return (b[0] - 48);
-		for  (i = 0; b[i] != '\0'; i++)
-		{
-			if (b[i] != '0' && b[i] != '1')
-				return (0);
-			for (j = len - 1; j > 0; j--)
-				pow = pow * base;
-			sum  = sum + (pow * (b[i] - 48));
-			len--;
-			pow = 1;
-		}
-		return (sum);
+	for (len = 0; b[len] != '\0'; len++)
+		;
+	if (len == 1 && (b[0] == '0' || b[0] == '1'))
+		return (b[0] - 48);
+	for  (i = 0; b[i] != '\0'; i++)
+	{
+		if (b[i] != '0' && b[i] != '1')
+			return (0);
+		for (j = len - 1; j > 0; j--)
+			pow = pow * base;
+		sum  = sum + (pow * (b[i] - 48));
+		len--;
+		pow = 1;
+	}
+	return (sum);
 }
--- 0x14-bit_manipulation/main.h
+++ 0x14-bit_manipulation/main.h
@@ -1,5 +1,7 @@
-#ifndef _MAIN.H_
-#define _MAIN.H_
+#ifndef _MAIN_H_ 
+#define _MAIN_H_ 
+
+#include <stdlib.h>
 
 unsigned int binary_to_uint(const char *b);
 void print_binary(unsigned long int n);
